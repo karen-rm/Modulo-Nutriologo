b8935b6c784352ce805d993c89e52f8ea4907fb1
erickGtz <erick.gutierrezsa@alumno.buap.mx>
2024-11-02 16:02:02 -0600

    tipo: feat [conexión realizada a la BD]

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
-import express from 'express';
+/*import express from 'express';
 
 const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});*/
+
+import express from 'express';
+import connection from './db.js'; // Importa la conexión de db.js
+
+const app = express();
+
+// Ruta para consultar la base de datos
+app.get('/api/data', async (req, res) => {
+  try {
+    const [rows] = await connection.query('SELECT * FROM nutriologo');
+    res.json(rows); // Devuelve los resultados en formato JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al consultar la base de datos' });
+  }
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
